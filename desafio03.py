9cfac620d31d3981efe629dcfd70c9d44eb17459
suporteagp <>
2025-01-06 17:51:20 -0300

    desafio 03

---
+++ desafio03.py
@@ -0,0 +1,20 @@
+# Integre na solução anterior um fluxo de While 
+# que repita o fluxo até que o usuário insira as 
+# informações corretas
+
+# Solicita ao usuário que digite seu nome
+
+
+        # Verifica se o nome está vazio
+
+        # Verifica se há números no nome
+
+# Solicita ao usuário que digite o valor do seu salário e converte para float
+
+
+# Solicita ao usuário que digite o valor do bônus recebido e converte para float
+
+
+# Exemplo simples de KPI
+
+# Imprime as informações para o usuário
